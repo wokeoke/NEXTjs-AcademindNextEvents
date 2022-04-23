fc8310d10a882015ebee6f9ae89f9897c35e2f77
wokeoke <woke.oke@gmail.com>
2022-04-23 15:11:46 +0700

    Navigating to the 'Filtered Events' Page Progammatically

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '@/components/events/event-list';
 import EventSearch from '@/components/events/events-search';
 
 function HomePage() {
+  const router = useRouter();
   const featuredEvents = getFeaturedEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <div>
-      <EventSearch />
+      <EventSearch onSearch={findEventsHandler} />
       <EventList items={featuredEvents} />
     </div>
   );
