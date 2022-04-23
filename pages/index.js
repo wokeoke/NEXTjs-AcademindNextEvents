aa92c7d56906bcc921756ba908a689374f66c91f
wokeoke <woke.oke@gmail.com>
2022-04-23 14:57:05 +0700

    Adding a Filter Form for Filtering Events

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '@/components/events/event-list';
+import EventSearch from '@/components/events/events-search';
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
+      <EventSearch />
       <EventList items={featuredEvents} />
     </div>
   );
